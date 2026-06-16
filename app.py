17e09a6ce846aca66def0f8701e497533668c4a8
kh82you-lgtm <kh82.you@gmail.com>
2026-06-16 16:44:30 +0900

    내용수정

--- app.py
+++ app.py
@@ -3,15 +3,14 @@ import streamlit as st
 st.title(" 안녕!! 유지아 👋")
 st.markdown(
     """ 
-    This is a playground for you to try Streamlit and have fun. 
+    사랑하는 유지아~~~. 
 
-    **There's :rainbow[so much] you can build!**
+    **오늘도 :rainbow[고생했어요]**
     
-    We prepared a few examples for you to get started. Just 
-    click on the buttons above and discover what you can do 
-    with Streamlit. 
+    아빠 오늘 회식이야~~~ 일찍갈께~~~~
+    **:rainbow[사랑해]** 
     """
 )
 
-if st.button("Send balloons!"):
+if st.button("여기 눌러줘!"):
     st.balloons()
